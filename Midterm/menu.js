0fe49ca6bf52c7cd74db5f693a8ddb8c327d5eab
Cammie Reel <camreel@iu.edu>
2025-03-12 15:55:06 -0400

    Edited the table on html file and started declaring variables on js file

---
+++ Midterm/menu.js
@@ -0,0 +1,14 @@
+const menuList = document.getElementById("menu");
+const cartTable = document.getElementById("cart");
+const noItemsRow = document,getElementById("no-items");
+
+const menuItems = [
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+]
